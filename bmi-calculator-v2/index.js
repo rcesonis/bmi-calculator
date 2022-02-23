61fc1815e31ea153c11795367cfe5cb6da4ca619
rcesonis <rimvydas.cesonis@gmail.com>
2022-02-23 18:40:53 +0100

    Passed hardcoded arghuments,defined paramaters[4,5,6]

--- bmi-calculator-v2/index.js
+++ bmi-calculator-v2/index.js
@@ -1,3 +1,12 @@
+// Function to calculate BMI
+function calculateBmi(weight, height) {
+    console.log(`input to function ok? ${weight} and ${height}`);
+}
+
+
+
+
+
 function bmiCalculator() {
     // User input 
     const weightInKg = parseInt(process.argv[2]);
@@ -7,17 +16,14 @@ function bmiCalculator() {
     const gender = process.argv[6];
     
     // Testing user input
-    console.log(`Weight: ${weightInKg}`);
-    console.log(`Height: ${heightInM}`);
-    console.log(`Age: ${userAge}`);
-    console.log(`Daily exercise: ${isExcercising}`);
-    console.log(`Gender: ${gender}`);
+    // console.log(`Weight: ${weightInKg}`);
+    // console.log(`Height: ${heightInM}`);
+    // console.log(`Age: ${userAge}`);
+    // console.log(`Daily exercise: ${isExcercising}`);
+    // console.log(`Gender: ${gender}`);
+
+    calculateBmi(82, 1.79);
 }
-
-
-
-
-
 
 
 
